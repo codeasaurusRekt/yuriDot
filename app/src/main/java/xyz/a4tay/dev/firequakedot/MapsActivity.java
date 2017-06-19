0425d530168c42339724a49cce0cee5041302433
danielmontilla <dmontilla88@gmail.com>
2017-06-19 18:56:26 -0400

    Beginning work on proper Settings Activity.  Selecting Settings from the pull out menu will navigate to the Generic Settings page(s).  Will need to work on making those relevant from here.

--- app/src/main/java/xyz/a4tay/dev/firequakedot/MapsActivity.java
+++ app/src/main/java/xyz/a4tay/dev/firequakedot/MapsActivity.java
@@ -29,6 +29,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.*;
+import com.google.maps.android.clustering.ClusterManager;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -42,6 +43,7 @@ import java.util.Random;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
     private GoogleMap mMap;
+    private ClusterManager<MyItem> mClusterManager;
 
     public static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
@@ -63,6 +65,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
         }
+
+    private void setUpClusterer() {
+    // Position the map.
+    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
+
+    // Initialize the manager with the context and the map.
+    // (Activity extends context, so we can pass 'this' in the constructor.)
+    mClusterManager = new ClusterManager<MyItem>(this, mMap);
+
+    // Point the map's listeners at the listeners implemented by the cluster
+    // manager.
+//    mMap.setOnCameraIdleListener(mClusterManager);
+    mMap.setOnMarkerClickListener(mClusterManager);
+
+    // Add cluster items (markers) to the cluster manager.
+    addItems();
+    }
+
+    private void addItems()
+        {
+
+        // Set some lat/lng coordinates to start with.
+        double lat = 51.5145160;
+        double lng = -0.1270060;
+
+        // Add ten cluster items in close proximity, for purposes of this example.
+        for (int i = 0; i < 10; i++)
+            {
+            double offset = i / 60d;
+            lat = lat + offset;
+            lng = lng + offset;
+            MyItem offsetItem = new MyItem(lat, lng);
+            mClusterManager.addItem(offsetItem);
+            }
+        }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap)
---
+++ app/src/main/java/xyz/a4tay/dev/firequakedot/MyItem.java
@@ -0,0 +1,39 @@
+package xyz.a4tay.dev.firequakedot;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+/**
+ * Created by Daniel Montilla on 6/19/2017.
+ */
+public class MyItem implements ClusterItem
+    {
+private final LatLng mPosition;
+//private final String mTitle;
+//private final String mSnippet;
+
+public MyItem(double lat, double lng) {
+mPosition = new LatLng(lat, lng);
+}
+
+public MyItem(double lat, double lng, String title, String snippet) {
+mPosition = new LatLng(lat, lng);
+//mTitle = title;
+//mSnippet = snippet;
+}
+
+@Override
+public LatLng getPosition() {
+return mPosition;
+}
+
+//@Override
+//public String getTitle() {
+//return mTitle;
+//}
+
+//@Override
+//public String getSnippet() {
+//return mSnippet;
+//}
+}
